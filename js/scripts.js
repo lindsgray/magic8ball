7efcd29f7ee9a69d1b298dfc1ab0330237c91ab6
Linds Gray <lindsaygray11@gmail.com>
2017-10-02 23:27:56 -0700

    finished this ish

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
 // target your button and assign an ask function that outputs a random image to your page
+const askButton = document.querySelector('.btn');
+const eightBall = document.querySelector('#eightBall');
+const inputField = document.querySelector('input');
+
+let randomNumber = Math.floor(Math.random() * 19) + 1;
+
+askButton.addEventListener('click', () => {
+    if (!inputField.value) {
+        alert('Please type in a question.');
+    } else {
+        eightBall.src = `img/magic8ball_${randomNumber}.png`;
+        inputField.value = '';
+        randomNumber = Math.floor(Math.random() * 19) + 1;
+    }
+});
+
